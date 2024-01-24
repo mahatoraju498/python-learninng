7fbda37db7e492ed1ef547f80b29820d15cc65b5
mahatoraju498 <mahatoraju2054@gmail.com>
2024-01-24 15:31:05 +0545

    hello

---
+++ jan_3_00_python/1_operators.py
@@ -0,0 +1,19 @@
+#operators in pyhton 
+
+#Logical operators 
+#Relational Operators
+#Assignment Operators
+#Membership function
+
+
+
+
+#Assignament operator ( =, +=, -=, *=, /=)
+message = "hello world"
+
+
+
+
+#Membership Operaors ('in' and 'not in')
+#The result of membership operation is also True and False.
+#Membership oprators are used in the sequential data
